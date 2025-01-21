6abecf1337e83734caff28401dced17aced34e1b
John A Stevenson <jostev@bgs.ac.uk>
2025-01-21 00:35:59 +0000

    Add figures and workdir to script

--- src/climate_paper_v1/climate_paper_figs.py
+++ src/climate_paper_v1/climate_paper_figs.py
@@ -1,48 +1,56 @@
 from io import StringIO
+import matplotlib.pyplot as plt
 import os
-import pandas as pd
+work_Dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'work')
+
+import csv
 
 def ProcessFile(filename):
     print(filename)
-    if not filename.endswith('.txt'):
+    if filename.endswith('.txt') is False:
         # Ignore non-data files
         return
 
     # Define column names and missing value indicators
-    column_names = ['year', 'month', 'tmax', 'tmin', 'frost_days', 'rain_mm', 'sun']
-    missing_values = {'tmax': '---', 'tmin': '---', 'frost_days': '---', 'rain_mm': '---', 'sun': '---'}
+    columnNames = ['year', 'month', 'tmax', 'tmin', 'frost_days', 'rain_mm', 'sun']
+    mvs = {'tmax': '---', 'tmin': '---', 'frost_days': '---', 'rain_mm': '---', 'sun': '---'}
 
     # Create pseudo-file that has removed the Site closed lines from
     # any files that have it e.g. cwmystwythdata.txt.
-    cleaned_lines = []
+    clean = []
     with open('data/' + filename, 'r') as file:
-        for line in file:
-            if 'site closed' not in line.casefold():  # Skip lines containing 'Site closed'
-                cleaned_lines.append(line)
-    cleaned_pseudo_file = StringIO(''.join(cleaned_lines))
+        for lines in file:
+            if 'site closed' not in lines.casefold():  # Skip lines containing 'Site closed'
+                clean += [lines]
+    cleaned_pseudo_file = StringIO(''.join(clean))
 
     # Read the cleaned lines, skipping the first lines of metadata
     # and the "provisional" data from the end of the file.
-    df = pd.read_csv(cleaned_pseudo_file, sep='\s+', skiprows=8,
-                    names=column_names, na_values=missing_values,
-                    on_bad_lines='warn')
+    import pandas as pd
+    df = pd.read_csv(cleaned_pseudo_file, sep='\s+', skiprows=8, names=columnNames, na_values=mvs, on_bad_lines='warn')
+    print(len(df))
 
     # Create an index with the dates
-    df['date'] = pd.to_datetime(df[['year', 'month']].assign(day=1))
+    df['date'] = pd.to_datetime(df[['year', 'month']]
+                                      .assign(day=1))
     df.set_index('date', inplace=True)
     df.drop(columns=['year', 'month'], inplace=True)
 
     # Some data values have '*' or '#' appended to them.
     # Force these to numeric values, returning NaN if it isn't possible.
-    for column in column_names[2:]:
-        df[column] = pd.to_numeric(df[column], errors='coerce')
+    for c in columnNames[2:]:
+        df[c] = pd.to_numeric(df[c], errors='coerce')
     
-    max_temps_file.write(f"{filename.replace("data.txt", "")},{df['tmax'].mean()}\n")
+    f .write(f"{filename.replace("data.txt", "")},{df['tmax'].mean()}\n")
+    ax = df['tmax'].plot(kind='line')
+    ax.set_title(filename.replace('data.txt', ''))
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Temperature degC')
+    ax.get_figure().savefig(os.path.join(work_Dir, filename.replace('data.txt', '.png',
+                                                        )), dpi=90)
+    plt.close()
+f = open(os.path.join(work_Dir, 'max_temps.csv'), 'wt')
+f .write("location,max_temp\n")
 
-max_temps_file = open('max_temps.csv', 'wt')
-max_temps_file.write("location,max_temp\n")
-
-for filename in os.listdir('data'):
+for filename in os.listdir(os.path.join(os.path.dirname(work_Dir), 'data')):
     ProcessFile(filename)
-
-max_temps_file.close()
